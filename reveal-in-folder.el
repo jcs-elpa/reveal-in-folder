af2cb21ce5d7a99504ba5a1eacd6099e62b3ea72
jenchieh <jcs090218@gmail.com>
2019-12-01 12:32:33 +0800

    Bump version.'

--- reveal-in-folder.el
+++ reveal-in-folder.el
@@ -6,7 +6,7 @@
 ;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
 ;; Description: Reveal current file in folder.
 ;; Keyword: folder finder reveal file explorer
-;; Version: 0.0.1
+;; Version: 0.0.2
 ;; Package-Requires: ((emacs "24.3"))
 ;; URL: https://github.com/jcs090218/reveal-in-folder
 
