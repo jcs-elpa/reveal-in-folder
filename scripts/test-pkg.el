06c6ccc2c745689947031e9c148fc04af8ea33f4
Jen-Chieh <jcs090218@gmail.com>
2020-10-22 13:12:14 +0800

    Added CI scripts.

---
+++ scripts/test-pkg.el
@@ -0,0 +1,15 @@
+;;
+;; (@* "Prepare" )
+;;
+
+(load-file (expand-file-name "./scripts/pkg-prepare.el"))
+
+(jcs-ensure-package-installed '(f s) t)
+
+(load-file (expand-file-name "./reveal-in-folder.el"))
+
+;;
+;; (@* "Test" )
+;;
+
+(reveal-in-folder)
